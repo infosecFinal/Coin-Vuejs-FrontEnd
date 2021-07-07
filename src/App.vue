37c78526666ede658d5c885cc8799f5e0255b87d
hhhoh <hhhoh@naver.com>
2021-07-07 23:09:04 +0900

    first vue

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,71 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+  <div style="background-color: black">
+    <!-- <button @click="connect">채팅방 참여</button> -->
+    <input  v-model="message" type="text" @keyup.enter="sendMessage">
+    <div v-for="item in recvList" :key="item">
+      <h3>content: {{item.content}}</h3>
     </div>
-    <router-view/>
   </div>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+import Stomp from 'webstomp-client';
+import SockJS from 'sockjs-client';
 
-#nav {
-  padding: 30px;
+export default {
+  name: 'App',
+  data() {
+    return {
+      recvList: [],
+      message: ""
+    }
+  },
+  created() {
+    this.connect();
+  },
+  methods: {
+    sendMessage() {
+      this.send();
+      this.message = ''
+    },
+    send() {
+      console.log(this.stompClient.connected);
+      console.log("Send Message: " + this.message);
+      if(this.stompClient && this.stompClient.connected) {
+        const msg = {
+          name: this.message
+        };
+        console.log(msg);
+        this.stompClient.send("/app/hello",JSON.stringify(msg), {});
+      }
+    },
+    connect() {
+      const serverURL = "http://localhost:3333";
+      var socket = new SockJS(serverURL+'/gs-guide-websocket');
+      this.stompClient = Stomp.over(socket);
+      console.log(`소켓 연결을 시도합니다. 서버 주소: ${serverURL}`)
+      this.stompClient.connect({}, frame=>{
+        this.connected = true;
+        console.log('success connect socket', frame);
+        this.stompClient.subscribe('/topic/greetings', res=>{
+          console.log('message from subs', res.body);
+          console.log(JSON.parse(res.body));
+          this.recvList.push(JSON.parse(res.body));
+          console.log(this.recvList);
+        });
+      },
+      error => {
+        console.log('소켓 연결 실패', error);
+        this.connected = false;
+      }
+      );
+    },
+  },
 }
+</script>
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
+<style scoped>
+  h3 {
+    color: white;
+  }
 </style>
---
+++ src/components/printchat.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'printchat'
+}
+</script>
+
+<style>
+
+</style>
