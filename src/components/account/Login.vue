daa941e5f345910fc69bdefad2a43201ad4ed6ce
sooops <wjdtnqls313@naver.com>
2021-07-15 11:01:21 +0900

    login edit

--- src/components/account/Login.vue
+++ src/components/account/Login.vue
@@ -12,7 +12,6 @@
         </div>
     </div>
     <br>
-
     <div class="site-section">
         <div class="container">
             <div class="row">
@@ -22,8 +21,8 @@
                 <div class="col-md-12">
 
                     <form name="loginform" action="#" method="post">
-                        <div class="p-4 p-lg-4 border table">
-                            <div style="padding-left: 200px;">
+                        <div class="p-4 p-lg-4 border table" style="background-color:#f1f5f8">
+                            <div style="padding-left: 200px; \">
                                 <select  id="login" class="login" name="login">
                                     <option value="user" selected> 회원 </option>
                                     <option value="admin"> 관리자 </option>
@@ -33,40 +32,37 @@
                             <div class="form-group" style="padding-left: 200px">
                                 <div class="form-group row">                                            
                                     <div class="col-lg-7">
-                                        <label for="login_id" class="text-black">아이디</label>
-                                        <input type="text" class="form-control" id="login_id" name="login_id" v-model="login_id">
+                                        <label for="login_id" class="text-black ">아이디</label>
+                                        <input type="text" class="form-control input-field" id="login_id" name="login_id" v-model="login_id" placeholder="ID" style="border:none">
                                     </div>
                                 </div>
                                 <div class="form-group row">
                                     <div class="col-lg-7">
                                         <label for="login_pw" class="text-black">비밀번호</label>
-                                        <input type="password" class="form-control" id="login_pw" name="login_pw" v-model="login_pw">
+                                        <input type="password" class="form-control input-field" id="login_pw" name="login_pw" v-model="login_pw" placeholder="PW" style="border:none">
                                     </div>
                                 </div>
                             
                             <br>
                                  <div class="form-group row">
                                         <div class="col-md-3 wrapmid">
-                                            <b-button id="LoginBtn"  @click="approveUser" class="btn btn-block" variant="primary">로그인</b-button>
+                                            <b-button pill id="LoginBtn"  @click="approveUser" class="btn btn-block" variant="warning">로그인</b-button>
                                         </div>
 
                                      <div class="col-md-3 wrapmid">
-                                         <b-button href = "/register" variant="primary" class=" btn-block">회원가입</b-button>
+                                         <b-button pill href = "/register" variant="warning" class=" btn-block">회원가입</b-button>
                                      </div>
                                  </div>
                             </div>
                         </div>
                     </form>
                 </div>
-
             </div>
         </div>
     </div>
-
 </div>
   <!-- Default form login -->
 </template>
-
 
 <script>
 import { checkUser } from '@/service'
@@ -82,19 +78,16 @@ export default {
     methods: {
         ...mapMutations('account',[
             'setId',
-            'setPage',
-            'setLoginState'
+            'setPage'
         ]),
         async approveUser() {
             const resp = await checkUser({
                 login_id: this.login_id,
                 login_pw: this.login_pw
             });
-            console.log(resp);
             if(resp.data.data !== null) {
                 this.setId(this.login_id);
                 this.setPage('Mypage');
-                this.setLoginState(true);
                 this.$router.push({
                     path: '/board/free'
                 })
