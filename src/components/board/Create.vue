f44338d59ce1164c3f387c4ea8cbc262e06a93c1
hhhoh <hhhoh@naver.com>
2021-07-15 13:38:34 +0900

    multiple file upload & file delete

--- src/components/board/Create.vue
+++ src/components/board/Create.vue
@@ -9,11 +9,17 @@
       rows="3"
       max-rows="6"
     ></b-form-textarea>
+    <div v-if="files">
+            <span v-for="(file, idx) in files" :key=idx>{{file.origin_file_Name}}<button @click="deleteFileFromList(idx)">X</button>
+                <br>
+            </span>
+    </div>
     <b-form-file
       v-model="file1"
       :state="Boolean(file1)"
       placeholder="Choose a file or drop it here..."
       drop-placeholder="Drop file here..."
+      multiple
     ></b-form-file>
     <b-button @click="content_id ? update() : insert()">Save</b-button>
     <b-button @click="cancle">Cancle</b-button>
@@ -22,7 +28,7 @@
 
 <script>
 import {insertData, updateData, fetchDataById} from '@/service'
-import {insertFile} from '@/service/file/file.js'
+import {insertFile, getFilesInfo, deleteFile} from '@/service/file/file.js'
 import {mapGetters} from 'vuex'
 
 export default {
@@ -34,17 +40,18 @@ export default {
             content: '',
             file1: null,
             data: {},
+            files: [],
             formData: new FormData()
         }
     },
     async created() {
-        console.log("Nan: ", isNaN(this.content_id));
         if(!isNaN(this.content_id)) {
             const resp = await fetchDataById(this.content_id);
-            console.log(resp);
+            const files = await getFilesInfo(this.content_id);
             this.user_id = resp.data.data.user_id;
             this.title = resp.data.data.title;
-            this.content = resp.data.data.content
+            this.content = resp.data.data.content;
+            this.files = files.data.list;
         }
     },
     computed: {
@@ -57,6 +64,12 @@ export default {
             this.$router.push({
                 path: '/board/free'
             })
+        },
+        async deleteFileFromList(idx) {
+            let file = this.files[idx]
+            this.files.splice(idx, 1);
+            const resp = await deleteFile(file);
+            console.log(resp);
         },
         async insert() {
             const resp = await insertData({
@@ -86,8 +99,12 @@ export default {
             })
         },
         async fileUpload() {
+            console.log(this.file1);
             let formData = new FormData();
-            formData.append('files', this.file1);
+            for (let i = 0; i < this.file1.length; i++) {
+                formData.append('files', this.file1[i]);
+                console.log(i,": ", this.file1[i])
+            }
             const resp = await insertFile(formData,this.content_id?this.content_id:'new');
             console.log(resp);
         }
--- src/service/file/file.js
+++ src/service/file/file.js
@@ -3,6 +3,7 @@ import axios from "axios";
 const baseURL = "http://localhost:8083";
 
 export const insertFile = (data, board_id) => {
+    console.log("insertFIle: ", data, board_id);
     return axios.post(`${baseURL}/file/upload?board_id=${board_id}`, data);
 }
 
@@ -14,4 +15,8 @@ export const getFile = (id) => {
     return axios.get(`${baseURL}/file/download/${id}`, {
         headers: {responseType: 'arraybuffer'}
     })
+}
+
+export const deleteFile = (file_id) => {
+    return axios.post(`${baseURL}/file/delete`, file_id)
 }
