a8837439e6f27b5af34c4d8998f9280170280380
YOUNGSEO-PARK <irene0seo97@gmail.com>
2021-07-16 05:08:21 +0900

    user image update editing

--- src/components/board/Create.vue
+++ src/components/board/Create.vue
@@ -143,5 +143,10 @@ export default {
     text-align:center;
     vertical-align:middle;
 }
+.custom-file, .custom-file-input {
+    position: relative;
+    width: 100%;
+    height: calc(1.5em + .75rem + 2px);
+}
 
 </style>
