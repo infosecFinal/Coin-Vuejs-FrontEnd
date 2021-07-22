bc65b1b48a7c9616b0e93782ca06ff785e3fee38
sooops <wjdtnqls313@naver.com>
2021-07-22 20:35:12 +0900

    header show id

--- src/components/common/Header.vue
+++ src/components/common/Header.vue
@@ -13,13 +13,33 @@
         class="collapse navbar-collapse justify-content-end"
       >
         <b-navbar-nav>
+          <!-- <b-nav-item>{{getLoginId}}</b-nav-item> -->
           <b-nav-item to="/">Home</b-nav-item>
           <b-nav-item @click="loginActions">{{
             getLoginState?'Logout':'Login'
           }}</b-nav-item>
-          <b-nav-item :to="getLoginState ? '/mypage' : '/register'"
+          <!-- <b-nav-item :to="getLoginState ? '/mypage' : '/register'"
             >{{ getLoginState?'MyPage':'Register' }}
+          </b-nav-item> -->
+
+
+        <b-nav-item :to="getLoginState ? '/mypage' : '/register'"
+            style="font-weight: bold ;">{{ getLoginState? getLoginId+'님'+getImagePath:'Register' }}
           </b-nav-item>
+
+<!-- 
+        <b-nav-item-dropdown text= '{{getLoginState? getLoginId : 'Register'}}'>
+        </b-dropdown-item > -->
+
+          
+<!--           
+          <b-nav-item-dropdown :to="getLoginState ? '/mypage' : '/register'"
+            style="font-weight: bold ;">{{ getLoginState? getLoginId+'님':'Register' }}
+            <!-- <b-dropdown-item href="/mypage">MYPAGE<b-dropdown-item> -->
+            <!-- <b-dropdown-item @click="loginActions">{{getLoginState? 'Logout':'Login'}}</b-dropdown-item> -->
+          <!-- </b-nav-item-dropdown> --> 
+
+
           <!-- <b-nav-item-dropdown text="Board">
             <b-dropdown-item href="#">Notice</b-dropdown-item>
             <b-dropdown-item to="/board/free">Free</b-dropdown-item>
@@ -64,7 +84,9 @@ export default {
         ...mapGetters('account', [
             'getLoginState',
             'getLogout',
-            'getMypage'
+            'getMypage',
+            'getLoginId',
+            'getImagePath'
         ])
     },
     methods: {
